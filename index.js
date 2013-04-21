5d493612ada7d6602e16a1f24b7fb9dd7819e87e
Schabse Laks <Dev@SLaks.net>
2013-04-21 17:35:03 -0400

    Add basic emitter code

--- index.js
+++
@@ -1,9 +0,0 @@
-﻿/*jshint node: true, camelcase: true, eqeqeq: true, forin: true, immed: true, latedef: false, newcap: true, noarg: true, undef: true, globalstrict: true*/
-"use strict";
-
-exports.createLogger = function (module) {
-
-};
-exports.createSubpackageLogger = function (module, subPackageName) {
-
-};
--- logger.js
+++
@@ -1,2 +0,0 @@
-﻿/*jshint node: true, camelcase: true, eqeqeq: true, forin: true, immed: true, latedef: false, newcap: true, noarg: true, undef: true, globalstrict: true*/
-"use strict";
